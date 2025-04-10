bf6c22015affef8eac3da06af08c9672605cda60
Mrityunjay SIngh <mrityunjaybeniwal@gmail.com>
2025-04-11 03:53:40 +0530

    changes

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask,redirect,url_for,render_template
 app = Flask(__name__)
-
 
 @app.route('/')
 def welcome():
-    return "hi this is mj"
+    return render_template('index.html')
 
-@app.route('/members')
-def wtf():
-    return "omggee"
+
+@app.route('/click' , methods = ['POST','GET'])
+def click():
+    return render_template('searchplayers.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
